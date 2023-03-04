e6453416c225d83dca64461593cf084f028dafdd
user <Fauzia.sayyed89@gmail.com>
2023-03-04 23:56:12 +0530

    commit

---
+++ src/User_song/main.java
@@ -0,0 +1,19 @@
+package User_song;
+
+public abstract class main implements inter{
+
+
+
+		//Inheritance_opps concept
+		//Interface concept
+		
+		 public static void main(String[] args) {  
+			 
+			 Interface_implementation obj =new  Interface_implementation();
+			 
+			 obj.m1();
+			
+
+		}}
+
+
